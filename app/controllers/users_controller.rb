74aa8634d8f54c94a475e95f0d4bf359a9141493
Your Name <arai>
2019-03-28 11:24:07 +0000

    Finish []4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
      if @user.update(user_params)
-      redirect_to user_path(@user.id)
+        flash[:notice] = "successfully update"
+        redirect_to user_path(@user.id)
      else
       render :edit
      end
